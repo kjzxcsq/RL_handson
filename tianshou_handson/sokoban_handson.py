a51e7bc7fb78a6e1b5d2a4f55183f52e226d7f3b
kjzxcsq <1114764192@qq.com>
2022-10-24 22:15:20 +0800

    add sokoban

---
+++ tianshou_handson/sokoban_handson.py
@@ -0,0 +1,59 @@
+import gym
+import gym_sokoban
+import tianshou as ts
+import torch, numpy as np
+from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+from tianshou.utils import TensorboardLogger
+writer = SummaryWriter('log/dqn')
+logger = TensorboardLogger(writer)
+
+env = gym.make('Sokoban-v2')
+train_envs = ts.env.DummyVectorEnv([lambda: gym.make('Sokoban-v0') for _ in range(8)])
+test_envs = ts.env.DummyVectorEnv([lambda: gym.make('Sokoban-v0') for _ in range(100)])
+
+
+class Net(nn.Module):
+    def __init__(self, state_shape, action_shape):
+        super().__init__()
+        self.model = nn.Sequential(*[
+            nn.Conv2d(3, 16, 5, 2), nn.BatchNorm2d(16), nn.ReLU(inplace=True),
+            nn.Conv2d(16, 32, 5, 2), nn.BatchNorm2d(32), nn.ReLU(inplace=True),
+            nn.Flatten(),
+            nn.Linear(59200, 128), nn.ReLU(inplace=True),
+            nn.Linear(128, 128), nn.ReLU(inplace=True),
+            nn.Linear(128, 128), nn.ReLU(inplace=True),
+            nn.Linear(128, np.prod(action_shape))
+        ])
+    def forward(self, obs, state=None, info={}):
+        if not isinstance(obs, torch.Tensor):
+            obs = torch.tensor(obs, dtype=torch.float)
+        batch = obs.shape[0]
+        # logits = self.model(obs.view(batch, -1))
+        logits = self.model(obs.permute(0, 3, 1, 2))
+        return logits, state
+
+state_shape = env.observation_space.shape or env.observation_space.n
+action_shape = env.action_space.shape or env.action_space.n
+net = Net(state_shape, action_shape)
+optim = torch.optim.Adam(net.parameters(), lr=1e-3)
+
+policy = ts.policy.DQNPolicy(net, optim, discount_factor=0.9, estimation_step=3, target_update_freq=320)
+
+train_collector = ts.data.Collector(policy, train_envs, ts.data.VectorReplayBuffer(20000, 10), exploration_noise=True)
+test_collector = ts.data.Collector(policy, test_envs, exploration_noise=True)
+
+result = ts.trainer.offpolicy_trainer(
+    policy, train_collector, test_collector,
+    max_epoch=1, step_per_epoch=10000, step_per_collect=10,
+    update_per_step=0.1, episode_per_test=100, batch_size=64,
+    train_fn=lambda epoch, env_step: policy.set_eps(0.1),
+    test_fn=lambda epoch, env_step: policy.set_eps(0.05),
+    stop_fn=lambda mean_rewards: mean_rewards >= 10,
+    logger=logger)
+print(f'Finished training! Use {result["duration"]}')
+
+policy.eval()
+policy.set_eps(0.05)
+collector = ts.data.Collector(policy, env, exploration_noise=True)
+collector.collect(n_episode=1, render=1 / 35)
